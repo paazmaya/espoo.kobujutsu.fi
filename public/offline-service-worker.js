94a3961c6becabf18e6781774b988664d476498d
Juga Paazmaya <paazmaya@yahoo.com>
2017-09-29 15:01:27 +0300

    How about that Service Worker?

--- public/offline-service-worker.js
+++ public/offline-service-worker.js
@@ -1,52 +1,44 @@
 
-var cacheName = '20170915a';
+var cacheName = '20170929a';
 
 // index.html should be only used when offline, in order to have latest content always shown.
-var cacheFiles = [
-  '/index.html',
+var cacheFilesFirst = [
+  // '/index.html',
   '/favicon.ico',
   '/assets/default.css',
-  '/assets/espoon-yuishinkai-ry-yhdistyksen-saannot-2016-12-210.pdf',
-  '/assets/ohjaaja-jukka-300.jpg',
   '/assets/ohjaaja-jukka.jpg',
-  '/assets/ohjaaja-kari-300.jpg',
   '/assets/ohjaaja-kari.jpg',
-  '/assets/ohjaaja-kimmo-300.jpg',
   '/assets/ohjaaja-kimmo.jpg',
   '/assets/rkhsk-logo.png',
-  '/assets/rkhsk-nunchaku-1507.png',
   '/assets/rkhsk-nunchaku-754.png',
-  '/assets/rkhsk-nunchaku-square-1024.png',
   '/assets/yuishinkai-logo.png'
+];
+var cacheFilesSoon = [
+  '/assets/espoon-yuishinkai-ry-yhdistyksen-saannot-2016-12-210.pdf',
+  '/assets/ohjaaja-jukka-300.jpg',
+  '/assets/ohjaaja-kari-300.jpg',
+  '/assets/ohjaaja-kimmo-300.jpg',
+  '/assets/rkhsk-nunchaku-1507.png',
+  '/assets/rkhsk-nunchaku-square-1024.png'
 ];
 
 this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(cacheFiles);
+      cache.addAll(cacheFilesSoon);
+      return cache.addAll(cacheFilesFirst);
     }).catch(function(error) {
+      console.error('Getting everything in cache failed.');
       console.error(error);
     })
   );
 });
 
-// Primarily to to fetch from the network
+// Primarily use cache but fetch from the network when not found
 this.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      console.log('event.request.url', event.request.url);
-      console.log('resp', resp);
-      if (resp) {
-        return resp;
-      }
-      return fetch(event.request).then(function(response) {
-        return response;
-      }).catch(function(error) {
-        console.error(error);
-      });
-    }).catch(function() {
-      // Got offline?
-      return caches.match('/index.html');
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
@@ -54,13 +46,11 @@ this.addEventListener('fetch', function(event) {
 // Remove any caches that are not the current
 this.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          return caches.delete(key);
-        }
-
-        return true;
+    caches.keys().then(function(list) {
+      return Promise.all(list.filter(function(key) {
+        return key !== cacheName;
+      }).map(function(key) {
+        return caches.delete(key);
       }));
     })
   );

cc3fdedecd2673385db9e208aa7e384f346f9e8d
Juga Paazmaya <paazmaya@yahoo.com>
2017-10-23 18:08:14 +0300

    Tell when to meet

--- public/offline-service-worker.js
+++ public/offline-service-worker.js
@@ -1,5 +1,5 @@
 
-var cacheName = '20171004a';
+var cacheName = '20171023a';
 
 // index.html should be only used when offline, in order to have latest content always shown.
 var cacheFilesFirst = [

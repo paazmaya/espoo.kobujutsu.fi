ea105a690caf34d2d04a3bebd7afd415377c5bff
Juga Paazmaya <paazmaya@yahoo.com>
2018-05-08 11:15:36 +0300

    Wednesday confirmed

--- public/offline-service-worker.js
+++ public/offline-service-worker.js
@@ -1,5 +1,5 @@
 
-var cacheName = '20180424b';
+var cacheName = '20180508a';
 
 // index.html should be only used when offline, in order to have latest content always shown.
 var cacheFilesFirst = [
